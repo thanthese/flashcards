7e046dace42de1f0c65a21846b604390468524ea
Stephen Mann <thanthese@gmail.com>
2011-11-15 17:54:11 -0600

    basics 'first commit' stuff

--- src/flash/core.clj
+++ src/flash/core.clj
@@ -1,1 +1,15 @@
 (ns flash.core)
+
+(def sample-initial-board
+   {:session-num 0
+    :cards [{:consecutive-correct 0
+             :next-session 0
+             :question "a"
+             :answer 1}
+            {:consecutive-correct 0
+             :next-session 0
+             :question "b"
+             :answer 2}]})
+
+(defn -main [& args]
+ (println "hello world"))
